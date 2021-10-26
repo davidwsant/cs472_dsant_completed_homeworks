54a43461e9b221265f2f7ffdd3c6fe98e2071389
David Sant <dwsant@noordacom.org>
2021-10-26 11:33:13 -0600

    added momentum to MLP

--- MLP/mlp.py
+++ MLP/mlp.py
@@ -5,18 +5,14 @@ import random
 from sklearn.model_selection import train_test_split
 
 class MLP():
-	def __init__(self,max_iterations=500,shuffle=True, step_size = 1, no_improvement_break = None, hidden_nodes = 5, porportion_train=0.8, initial_weights_zero = False):
-		import copy
-		import numpy as np
-		import math
-		import random
-		from sklearn.model_selection import train_test_split
+	def __init__(self,max_iterations=500,shuffle=True, step_size = 0.1, no_improvement_break = None, hidden_nodes = 5, porportion_train=0.8, initial_weights_zero = False, momentum=0.2):
 		self.max_iterations = max_iterations
 		self.no_improvement_break = no_improvement_break
 		self.shuffle=shuffle
 		self.step_size = step_size
 		self.porportion_train = porportion_train
 		self.initial_weights_zero = initial_weights_zero
+		self.momentum = momentum
 		self.epoch_number = 0
 		self.rmse = []
 		self.training_rmse = []
@@ -51,10 +47,14 @@ class MLP():
 				first_hidden_deltas.append(individual_delta)
 
 			# now calculate the new weights
-			new_hidden_to_output = np.outer(first_hidden_z_values, delta_outputs)*self.step_size+self.weight_matrices[1]
+			change_in_hidden_to_output = np.outer(first_hidden_z_values, delta_outputs)*self.step_size
+			new_hidden_to_output = change_in_hidden_to_output+self.weight_matrices[1]+self.momentum*self.previous_change[1]
 			self.weight_matrices[1] = new_hidden_to_output
-			new_input_to_hidden = np.outer(entry[0], first_hidden_deltas)*self.step_size+self.weight_matrices[0]
+			self.previous_change[1] = change_in_hidden_to_output
+			change_in_input_to_hidden = np.outer(entry[0], first_hidden_deltas)*self.step_size
+			new_input_to_hidden = change_in_input_to_hidden+self.weight_matrices[0]+self.momentum*self.previous_change[0]
 			self.weight_matrices[0] = new_input_to_hidden
+			self.previous_change[0] = change_in_input_to_hidden
 		# Now that the epoch has run, find out the root mean squared error to determine if it needs to run again
 		sse = 0
 		for entry in self.test_data:
@@ -69,8 +69,8 @@ class MLP():
 		self.rmse.append(individual_rmse)
 		if not self.best_rmse or individual_rmse < self.best_rmse:
 			self.best_rmse = individual_rmse
-			self.best_epoch = self.epoch_number
-			self.best_weights = self.weight_matrices
+			self.best_epoch = copy.deepcopy(self.epoch_number)
+			self.best_weights = copy.deepcopy(self.weight_matrices)
 		# Save the training RMSE values as well. These will not be used as stopping criteria
 		training_sse = 0
 		for entry in self.train_data:
@@ -95,9 +95,6 @@ class MLP():
 			tmp_vector = copy.deepcopy(zero_vector)
 			tmp_vector[i] = 1
 			label_to_matrix_dictionary[label] = tmp_vector
-		label_matrix = []
-		for entry in input_labels:
-			label_matrix.append(label_to_matrix_dictionary[entry])
 
 		values_train, values_test, labels_train, labels_test = train_test_split(input_values, input_labels, train_size=self.porportion_train)
 		# Change out the labels to matrices
@@ -122,12 +119,17 @@ class MLP():
 		self.weight_matrices = []
 		self.weight_matrices.append(np.random.rand(len(self.train_data[0][0]), self.hidden_nodes))
 		self.weight_matrices.append(np.random.rand(self.hidden_nodes + 1, len(self.train_data[0][1])))
+		# save the change in weights so that you can change the momentum
+		# same shape as the weight_matrices, except everything needs to start as zero
+		self.previous_change = copy.deepcopy(self.weight_matrices)
+		for i in range(len(self.previous_change)):
+			for j in range(len(self.previous_change[i])):
+				for k in range(len(self.previous_change[i][j])):
+					self.previous_change[i][j][k] = 0
 		if self.initial_weights_zero:
-			# Just for class, start everything with zero instead of the random values
-			for i in range(len(self.weight_matrices)):
-				for j in range(len(self.weight_matrices[i])):
-					for k in range(len(self.weight_matrices[i][j])):
-						self.weight_matrices[i][j][k] = 0
+			self.weight_matrices = copy.deepcopy(self.previous_change)
+			# Just for BYU class, start everything with zero instead of the random values
+
 		while self.epoch_number < self.max_iterations:
 			if self.no_improvement_break and (self.epoch_number - self.no_improvement_break) >= self.best_epoch:
 				# If it has a specified no improvement break number
@@ -175,6 +177,7 @@ class MLP():
 			first_hidden_z_values = get_z(first_hidden_net_values) + [1] # add the bias weight
 			output_net_values = np.matmul(first_hidden_z_values, self.weight_matrices[1])
 			output_z_values.append(get_z(output_net_values))
+		self.output_z_values = output_z_values
 		confidence_matrix = []
 		for entry in output_z_values:
 			individual_confidence_matrix = []
